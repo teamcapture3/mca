aed38750e3e0dc07e2e03b5b4317f2cc4e0aae59
PainKiller3 <ninadpatil100@gmail.com>
2019-11-20 19:58:16 +0530

    Updated Sql Files For C++
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- Sem 1/C++/sql files/include/mysql_time.h
+++ Sem 1/C++/sql files/include/mysql_time.h
@@ -1,8 +1,9 @@
-/* Copyright (c) 2004, 2011, Oracle and/or its affiliates. All rights reserved.
+/* Copyright (C) 2004 MySQL AB & MySQL Finland AB & TCX DataKonsult AB
 
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
- the Free Software Foundation; version 2 of the License.
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
 
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -11,7 +12,7 @@
 
  You should have received a copy of the GNU General Public License
  along with this program; if not, write to the Free Software
- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA */
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
 
 #ifndef _mysql_time_h_
 #define _mysql_time_h_
@@ -47,7 +48,7 @@ enum enum_mysql_timestamp_type
 typedef struct st_mysql_time
 {
   unsigned int  year, month, day, hour, minute, second;
-  unsigned long second_part;  /**< microseconds */
+  unsigned long second_part;
   my_bool       neg;
   enum enum_mysql_timestamp_type time_type;
 } MYSQL_TIME;
--- Sem 1/C++/sql files/include/mysql_version.h
+++ Sem 1/C++/sql files/include/mysql_version.h
@@ -1,5 +1,4 @@
-/* Copyright Abandoned 1996, 1999, 2001 MySQL AB, 2013 Oracle and/or
-   its affiliates.
+/* Copyright Abandoned 1996, 1999, 2001 MySQL AB
    This file is public domain and comes with NO WARRANTY of any kind */
 
 /* Version numbers for protocol & mysqld */
@@ -9,16 +8,16 @@
 #ifdef _CUSTOMCONFIG_
 #include <custom_conf.h>
 #else
-#define PROTOCOL_VERSION           10
-#define MYSQL_SERVER_VERSION       "5.7.2-m12"
-#define MYSQL_VERSION_ID           50702
-#define LIBMYSQL_VERSION           "6.1.0"
-#define LIBMYSQL_VERSION_ID        60100
-#define MYSQL_PORT                 3306
-#define MYSQL_PORT_DEFAULT	       0
-#define MYSQL_UNIX_ADDR            "/tmp/mysql.sock"
-#define MYSQL_CONFIG_NAME          "my"
-#define MYSQL_COMPILATION_COMMENT	"MySQL Connector/C (GPL)"
+#define PROTOCOL_VERSION		10
+#define MYSQL_SERVER_VERSION		"5.0.16"
+#define MYSQL_BASE_VERSION		"mysqld-5.0"
+#define MYSQL_SERVER_SUFFIX_DEF	""
+#define FRM_VER				6
+#define MYSQL_VERSION_ID		50016
+#define MYSQL_PORT			3306
+#define MYSQL_UNIX_ADDR			"/tmp/mysql.sock"
+#define MYSQL_CONFIG_NAME		"my"
+#define MYSQL_COMPILATION_COMMENT	"Official MySQL binary"
 
 /* mysqld compile time options */
 #endif /* _CUSTOMCONFIG_ */
